1f1eba49c0aa91caf9f8af5e30cf5c2d0d0a272c
Protyush Ghorui <ghorui.protyushraj@gmail.com>
2025-11-13 22:16:02 +0530

    changed spinner ui

--- project-third-year/client/recommendation-system/src/components/common/LoadingSpinner.js
+++ project-third-year/client/recommendation-system/src/components/common/LoadingSpinner.js
@@ -1,101 +1,274 @@
-import React from 'react';
+import React from "react";
 
 const LoadingSpinner = () => {
-    // CSS for the loader container and SVG animations
-    const componentStyles = `
-        .loader-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            min-height: 100vh;
-            background-color: #141414; /* Match your app's background */
-        }
-        .loader {
-            display: flex;
-            align-items: center;
-            position: relative;
-        }
-        /* Adjusted width/height slightly if needed */
-        .loader svg.letter {
-            margin: 0 2px; /* Slightly reduce margin for more letters */
-            width: 48px;   /* Slightly smaller letters */
-            height: 48px;
-        }
-        .absolute {
-            position: absolute;
-        }
-        .dash {
-            animation:
-                dashArray 2s ease-in-out infinite,
-                dashOffset 2s linear infinite;
-        }
-        @keyframes dashArray {
-            0% { stroke-dasharray: 0 1 359 0; }
-            50% { stroke-dasharray: 0 359 1 0; }
-            100% { stroke-dasharray: 359 1 0 0; }
-        }
-        @keyframes dashOffset {
-            0% { stroke-dashoffset: 365; }
-            100% { stroke-dashoffset: 5; }
-        }
-        .gradient-b { stroke: url(#b); }
-        .gradient-c { stroke: url(#c); }
-        .gradient-d { stroke: url(#d); }
-    `;
-
-    return (
-        <div className="loader-container">
-            <style>{componentStyles}</style>
-            <div className="loader">
-                {/* SVG Definitions (Gradients) - These remain the same */}
-                <svg className="absolute" width="0" height="0">
-                    <defs>
-                        <linearGradient id="b" x1="0" y1="62" x2="0" y2="2" gradientUnits="userSpaceOnUse">
-                            <stop stopColor="#973BED"></stop> <stop offset="1" stopColor="#007CFF"></stop>
-                        </linearGradient>
-                        <linearGradient id="c" x1="0" y1="64" x2="0" y2="0" gradientUnits="userSpaceOnUse">
-                            <stop stopColor="#FFC800"></stop> <stop offset="1" stopColor="#F0F"></stop>
-                            <animateTransform attributeName="gradientTransform" type="rotate" values="0 32 32;-270 32 32;-540 32 32;-810 32 32;-1080 32 32" dur="8s" keyTimes="0;0.125;0.25;0.375;0.5;0.625;0.75;0.875;1" repeatCount="indefinite"></animateTransform>
-                        </linearGradient>
-                        <linearGradient id="d" x1="0" y1="62" x2="0" y2="2" gradientUnits="userSpaceOnUse">
-                            <stop stopColor="#00E0ED"></stop> <stop offset="1" stopColor="#00DA72"></stop>
-                        </linearGradient>
-                    </defs>
-                </svg>
-
-                {/* --- NEW SVG Letters for "LOADING" --- */}
-                {/* L */}
-                <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M5 5 V49 H39" className="dash gradient-b" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                {/* O */}
-                 <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M22,5 C10,5 5,15 5,27 S10,49 22,49 S39,39 39,27 S34,5 22,5 Z" className="dash gradient-c" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                 {/* A */}
-                 <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M5 49 L22 5 L39 49 M12 35 H32" className="dash gradient-d" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                 {/* D */}
-                 <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M5 5 V49 H25 C35,49 39,39 39,27 S35,5 25,5 H5 Z" className="dash gradient-b" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                 {/* I */}
-                 <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M22 5 V49" className="dash gradient-c" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                 {/* N */}
-                 <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M5 49 V5 L39 49 V5" className="dash gradient-d" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                 {/* G */}
-                 <svg className="letter" viewBox="0 0 44 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                   <path pathLength="360" d="M39 27 C39,15 34,5 22,5 S5,15 5,27 S10,49 22,49 H30 V35 H22" className="dash gradient-b" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-                {/* --- End of SVG Letters --- */}
+  return (
+    <div className="fullscreen-loader">
+      {/* Spiderman Loader */}
+      <div className="container center">
+        <div className="rope center">
+          <div className="legs center">
+            <div className="boot-l"></div>
+            <div className="boot-r"></div>
+          </div>
+          <div className="costume center">
+            <div className="spider">
+              <div className="s1 center"></div>
+              <div className="s2 center"></div>
+              <div className="s3"></div>
+              <div className="s4"></div>
             </div>
+            <div className="belt center"></div>
+            <div className="hand-r"></div>
+            <div className="hand-l"></div>
+            <div className="neck center"></div>
+            <div className="mask center">
+              <div className="eye-l"></div>
+              <div className="eye-r"></div>
+            </div>
+            <div className="cover center"></div>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* ALL CSS */}
+      <style jsx>{`
+        .fullscreen-loader {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          background: rgba(0, 0, 0, 0.95);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          z-index: 9999; /* on top of everything */
+        }
+
+        .center {
+          position: absolute;
+          transform: translateX(-50%);
+          left: 50%;
+        }
+
+        .container {
+          height: 31.25em;
+          width: 21.87em;
+        }
+
+        .rope {
+          height: 13.62em;
+          width: 0.15em;
+          background-color: #ffffff;
+          animation: swing 2s infinite;
+        }
+
+        @keyframes swing {
+          50% {
+            transform: translateY(-4em);
+          }
+        }
+
+        .legs {
+          height: 1.12em;
+          width: 7.5em;
+          box-shadow: 0 0 0 0.12em #140243, 0 0 0 1.06em #1b1676,
+            0 0 0 1.18em #140243;
+          top: 12.5em;
+          border-radius: 3.12em;
+        }
+
+        .boot-l,
+        .boot-r {
+          height: 1.25em;
+          width: 2.5em;
+          background-color: #e32832;
+          position: absolute;
+          border: 0.12em solid #140243;
+          bottom: 1.12em;
+        }
+
+        .boot-l {
+          left: 1em;
+        }
+
+        .boot-r {
+          transform: rotateY(180deg);
+          left: 3.81em;
+        }
+
+        .boot-l:before,
+        .boot-r:before {
+          content: "";
+          position: absolute;
+          border-bottom: 1.12em solid #140243;
+          border-left: 1.18em solid transparent;
+          bottom: 1.31em;
+          left: 1.46em;
+        }
+
+        .boot-l:after,
+        .boot-r:after {
+          content: "";
+          position: absolute;
+          border-bottom: 1.12em solid #e32832;
+          border-left: 1em solid transparent;
+          right: 0;
+          top: -0.93em;
+        }
+
+        .costume {
+          height: 6.25em;
+          width: 5.62em;
+          background: linear-gradient(
+            to right,
+            #1b1676 20%,
+            #e32832 20%,
+            #e32832 80%,
+            #1b1676 80%
+          );
+          border: 0.12em solid #140243;
+          top: 14.68em;
+        }
+
+        .spider {
+          height: 1.87em;
+          width: 0.93em;
+          background-color: #140243;
+          border-radius: 45%;
+          position: absolute;
+          transform: translate(-50%, -50%);
+          top: calc(50% + 0.93em);
+          left: 50%;
+        }
+
+        .s1,
+        .s3 {
+          height: 7.5em;
+          width: 2.5em;
+          border-radius: 0 0 1.37em 1.37em;
+          border-bottom: 0.12em solid #140243;
+          position: absolute;
+        }
+
+        .s2,
+        .s4 {
+          height: 7.5em;
+          width: 3.12em;
+          border-radius: 0 0 1.56em 1.56em;
+          border-bottom: 0.12em solid #140243;
+          position: absolute;
+        }
+
+        .s1,
+        .s2,
+        .s3,
+        .s4 {
+          left: 50%;
+        }
+
+        .s1 {
+          bottom: 1.43em;
+        }
+
+        .s2 {
+          bottom: 0.93em;
+        }
+
+        .s3,
+        .s4 {
+          transform: translateX(-50%) rotateX(180deg);
+        }
+
+        .s3 {
+          top: 1.43em;
+        }
+
+        .s4 {
+          top: 0.93em;
+        }
+
+        .belt {
+          height: 0.43em;
+          width: 5.87em;
+          background-color: #e32832;
+          border: 0.12em solid #140243;
+          top: -0.12em;
+        }
+
+        .hand-r,
+        .hand-l {
+          height: 8.12em;
+          width: 1.12em;
+          border-radius: 1.25em;
+          background: linear-gradient(
+            #e32832 4.6em,
+            #140243 4.6em,
+            #140243 4.75em,
+            #1b1676 4.75em,
+            #1b1676 8.12em
+          );
+          border: 0.12em solid #140243;
+          position: absolute;
+          transform-origin: bottom;
+          bottom: -0.12em;
+        }
+
+        .hand-r {
+          right: -0.75em;
+          transform: rotate(-22deg);
+        }
+
+        .hand-l {
+          left: -0.75em;
+          transform: rotate(22deg);
+        }
+
+        .neck {
+          height: 0.37em;
+          width: 1.25em;
+          background-color: #e32832;
+          bottom: -0.62em;
+          border: 0.12em solid #140243;
+        }
+
+        .mask {
+          height: 4.65em;
+          width: 4.06em;
+          background-color: #e32832;
+          border-radius: 50%;
+          border: 0.12em solid #140243;
+          top: 6.56em;
+        }
+
+        .eye-l,
+        .eye-r {
+          height: 0.68em;
+          width: 1.37em;
+          background-color: #ffffff;
+          border-radius: 1.37em 1.37em 0 0;
+          border: 0.12em solid #140243;
+          position: absolute;
+          top: 2.1em;
+        }
+
+        .eye-l {
+          left: 0.3em;
+        }
+
+        .eye-r {
+          right: 0.3em;
+        }
+
+        .cover {
+          height: 3.12em;
+          width: 0.15em;
+          background-color: #ffffff;
+          bottom: 8.12em;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
